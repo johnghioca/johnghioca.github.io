5eb512e36044134da2435d4ceec5112b93610033
John Ghioca <john.ghioca@akqa.com>
2025-06-20 17:23:10 -0400

    fix: out folder

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", // <=== enables static exports
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
 };
 
