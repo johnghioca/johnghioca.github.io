146b8c637709f465399ed1b0bc9448c7c6f7124f
John Ghioca <john.ghioca@akqa.com>
2025-06-23 12:36:19 -0400

    Revert to 13c886e759826e4a87749dde3ad714b4328c5a9b

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: "export", // <=== enables static exports
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+};
+
+export default nextConfig;
