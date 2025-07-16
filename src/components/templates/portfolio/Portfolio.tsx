1138473a7a0312fdbb509cd7c3224f0cbf573c80
John Ghioca <john.ghioca@akqa.com>
2025-07-16 16:58:59 -0400

    feat: reduce stack size and fling

--- src/components/templates/portfolio/Portfolio.tsx
+++ src/components/templates/portfolio/Portfolio.tsx
@@ -30,7 +30,7 @@ const Portfolio = () => {
   ];
   const to = (i: number) => ({
     x: 0,
-    y: i * -4,
+    y: i * -2,
     scale: 1,
     rot: -10 + Math.random() * 20,
     delay: i * 100,
@@ -50,13 +50,13 @@ const Portfolio = () => {
   // Create a gesture, we're interested in down-state, delta (current-pos - click-pos), direction and velocity
   const bind = useDrag(
     ({ args: [index], down, movement: [mx], direction: [xDir], velocity }) => {
-      const trigger = velocity > 0.05; // If you flick hard enough it should trigger the card to fly out
+      const trigger = velocity > 0.02; // If you flick hard enough it should trigger the card to fly out
       const dir = xDir < 0 ? -1 : 1; // Direction should either point left or right
       if (!down && trigger) gone.add(index); // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
       api.start((i) => {
         if (index !== i) return; // We're only interested in changing spring-data for the current spring
         const isGone = gone.has(index);
-        const x = isGone ? (200 + window.innerWidth) * dir : down ? mx : 0; // When a card is gone it flys out left or right, otherwise goes back to zero
+        const x = isGone ? (150 + window.innerWidth) * dir : down ? mx : 0; // When a card is gone it flys out left or right, otherwise goes back to zero
         const rot = mx / 100 + (isGone ? dir * 10 * velocity : 0); // How much the card tilts, flicking it harder makes it rotate faster
         const scale = down ? 1.1 : 1; // Active cards lift up a bit
         return {
