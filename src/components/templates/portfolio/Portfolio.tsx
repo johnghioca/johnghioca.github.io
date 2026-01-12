27c2f838904f768af2e1604b358bb90492695d75
John Ghioca <john.ghioca@akqa.com>
2026-01-12 11:38:50 -0500

    refactor(Portfolio): simplify card movement logic in animation

--- src/components/templates/portfolio/Portfolio.tsx
+++ src/components/templates/portfolio/Portfolio.tsx
@@ -56,7 +56,13 @@ const Portfolio = () => {
       api.start((i) => {
         if (index !== i) return; // We're only interested in changing spring-data for the current spring
         const isGone = gone.has(index);
-        const x = isGone ? (150 + window.innerWidth) * dir : down ? mx : 0; // When a card is gone it flys out left or right, otherwise goes back to zero
+        // When a card is gone it flys out left or right, otherwise goes back to zero
+        let x = 0;
+        if (isGone) {
+          x = (150 + window.innerWidth) * dir;
+        } else if (down) {
+          x = mx;
+        }
         const rot = mx / 100 + (isGone ? dir * 10 * velocity : 0); // How much the card tilts, flicking it harder makes it rotate faster
         const scale = down ? 1.1 : 1; // Active cards lift up a bit
         return {
