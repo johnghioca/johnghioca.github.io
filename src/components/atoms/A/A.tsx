97a3e5e6f2ac5295e11a6b490ff5ab675d7e0ff1
John Ghioca <john.ghioca@akqa.com>
2026-01-21 16:14:46 -0500

    refactor(A): simplify className management using tailwind-merge

--- src/components/atoms/A/A.tsx
+++ src/components/atoms/A/A.tsx
@@ -1,9 +1,19 @@
+import { twMerge } from "tailwind-merge";
+
 type AProps = React.ComponentPropsWithoutRef<"a">;
 
 const A: React.FC<AProps> = ({ className = "", children, ...props }) => {
 	return (
 		<a
-			className={`block bg-[#069] hover:bg-[#00a0dc] focus:bg-[#00a0dc] h-12 leading-12 font-bold text-center transition-[background] duration-600 border-b-black border-b border-solid xl:bg-transparent xl:border-0 xl:h-16 xl:leading-16 -outline-offset-1 xl:rounded-sm xl:hover:bg-transparent xl:focus:bg-transparent xl:hover:text-[#00a0dc] xl:focus:text-[#00a0dc] ${className}`}
+			className={twMerge(
+				"block h-12 leading-12 font-bold text-center border-b border-solid -outline-offset-1",
+				"bg-[#069] border-b-black xl:bg-transparent", // base color styles
+				"transition-[background] duration-600", // transition styles
+				"hover:bg-[#00a0dc] xl:hover:bg-transparent xl:hover:text-[#00a0dc]", // hover styles
+				"focus:bg-[#00a0dc] xl:focus:bg-transparent xl:focus:text-[#00a0dc]", // focus styles
+				"xl:h-16 xl:leading-16 xl:rounded-sm xl:border-0", // xl styles
+				className,
+			)}
 			{...props}
 		>
 			{children}
