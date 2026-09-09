e0a3ea4220049fe2a4442371891da1277477971b
John Ghioca <john.ghioca@akqa.com>
2026-09-09 14:22:36 -0400

    refactor: test name

--- src/components/atoms/Logo/Logo.test.tsx
+++ src/components/atoms/Logo/Logo.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import { expect, test } from "vitest";
 import { Logo } from "./Logo";
 
-test("Page", () => {
+test("Logo component renders", () => {
 	render(<Logo>John Ghioca</Logo>);
 	expect(screen.getByRole("heading", { level: 1, name: "John Ghioca" })).toBeDefined();
 });
