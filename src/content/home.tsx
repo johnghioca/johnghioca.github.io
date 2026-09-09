53061594da813e12d516de27446a1fdaebb3a65c
John Ghioca <john.ghioca@akqa.com>
2026-09-09 18:00:44 -0400

    fix: format

--- src/content/home.tsx
+++ src/content/home.tsx
@@ -68,9 +68,10 @@ const homeContent: HomeContent = {
 				I&apos;m a full-stack web developer focused on building modern,
 				high-performance websites and applications using the{" "}
 				<span className="text-[#00a0dc]">JAMstack</span>, including Next.js,
-				React, TypeScript, Supabase, and headless CMS platforms.
-				I use these technologies to create fast, scalable, and engaging digital experiences. 
-				Check out some of the companies I&apos;ve had the opportunity to work with below.
+				React, TypeScript, Supabase, and headless CMS platforms. I use these
+				technologies to create fast, scalable, and engaging digital experiences.
+				Check out some of the companies I&apos;ve had the opportunity to work
+				with below.
 			</>
 		),
 	},
